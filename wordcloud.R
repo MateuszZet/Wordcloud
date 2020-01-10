d496e5951ccada6b96ed4f336ba778d2b86a215e
MateuszZet <mattheus.zet@gmail.com>
2020-01-10 17:19:22 +0100

    Update wordcloud.R

--- wordcloud.R
+++ wordcloud.R
@@ -1,30 +1,32 @@
 
 rquery.wordcloud <- function(x, lang = "english", colorPalette = "Dark2", min.freq=10, max.words = 100) {
 
-
-
 	#all necessery libriaries
 	library("tm")
 	library("wordcloud")
-	library("SnowballC")
 	library("RColorBrewer")
-
 
 	#read from file
 	text <- readLines(x)
 
 	# Load the text as a corpus
 	docs <- Corpus(VectorSource(text))
+	
 	# Convert the text to lower case
 	docs <- tm_map(docs, content_transformer(tolower))
+	
 	# Remove numbers
 	docs <- tm_map(docs, removeNumbers)
+	
 	# Remove stopwords for the language 
 	docs <- tm_map(docs, removeWords, stopwords(lang))
+	
 	# Remove punctuations
 	docs <- tm_map(docs, removePunctuation)
+	
 	# Eliminate extra white spaces
 	docs <- tm_map(docs, stripWhitespace)
+	
 	# Create term-document matrix
 	tdm <- TermDocumentMatrix(docs)
 	m <- as.matrix(tdm)
@@ -36,10 +38,9 @@ rquery.wordcloud <- function(x, lang = "english", colorPalette = "Dark2", min.fr
 	if (!colorPalette %in% rownames(brewer.pal.info)) colors = colorPalette
 	else colors = brewer.pal(8, colorPalette)
 
-
 	# Plot the word cloud
 	set.seed(1234)
-	# WorldCloud documentation: https://www.rdocumentation.org/packages/wordcloud/versions/2.6/topics/wordcloud
+	# WordCloud documentation: https://www.rdocumentation.org/packages/wordcloud/versions/2.6/topics/wordcloud
 	wordcloud(d$word, d$freq, min.freq = min.freq, max.words = max.words,
 			random.order = FALSE, rot.per = 0.35,
 			use.r.layout = FALSE, colors = colors)
